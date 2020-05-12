996251ec1c9bd95c76bfac7d6237b0dc7b092d3f
Fagner Nascimento <fagner@novatera.com.br>
2020-05-12 09:34:31 -0300

    Add files via upload
    Alguns Comandos Uteis de Powershell para administração de Windows

---
+++ Comandos-Uteis01.ps1
@@ -0,0 +1,13 @@
+﻿# Ver Marca e Modelos do Computador Local
+
+Get-WmiObject -Class Win32_ComputerSystem
+
+#Exibir informações sobre a BIOS do computador
+#Além de ver informações sobre o computador, você também pode conseguir informações sobre a BIOS do seu computador através do PowerShell. 
+#Para isso, basta abrir o PowerShell e executar o comando abaixo:
+
+Get-WmiObject -Class Win32_BIOS -ComputerName LAP-SUPORTE
+
+#Listar os 5 processos que estão usando mais memória
+
+ps | sort –p ws | select –last 10
