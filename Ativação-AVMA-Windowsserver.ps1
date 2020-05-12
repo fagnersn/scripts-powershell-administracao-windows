3a64f4c3c1d0be46999aa22f063d1281badb7ddd
Fagner Nascimento <fagner@novatera.com.br>
2020-05-12 09:32:47 -0300

    Add files via upload
    Comandos para ativação de Maquina Virtual AVMA

---
+++ Ativação-AVMA-Windowsserver.ps1
@@ -0,0 +1,20 @@
+﻿## Comandos de Ativação do Windows ##
+
+# Inserindo uma Chave no Windows
+
+ slmgr /ipk AVMAkey 
+
+ # Chaves AVMA Key
+
+# TABELA 1-1 Chaves AVMA
+#Windows Server 2016  Datacenter:    TMJ3Y-NTRTM-FJYXT-T22BY-CWG3J
+#Windows Server 2016 Standard:       C3RCX-M6NRP-6CXC9-TW2F2-4RHYD
+#Windows Server 2016  Essentials :   B4YNW-62DX9-W8V6M-82649-MHBKQ
+
+#Windows Server 2012 R2 Datacenter :  Y4TGP-NPTV9-HTC2H-7MGQ3-DV4TW
+#Windows Server 2012 R2 Standard  :   DBGBW-NPF86-BJVTX-K3WKJ-MTB6V
+#Windows Server 2012 R2 Essentials :  K2XGM-NMBT3-2R6Q8-WF2FK-P36R2 
+
+##  Portas a Serem liberadas para Servidors KMS 80,443,1688
+##  Registro KMS no DNS  SRV Record (_VLMCS) on DNS
+
